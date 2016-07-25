36cd7c8175663d65211b999f6500f6d083ca25c3
吴雨楠 <wuyunan@wuyunandemacbook-air.local>
2016-07-25 14:25:09 -0500

    Finished setting up Table View

--- College Profile Builder(Part A)/ViewController.swift
+++ College Profile Builder(Part A)/ViewController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var tableView: UITableView!
+    var universities = ["University of Texas at Austin", "University of Illinois", "University of Washington, Seattle"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return universities.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
+        cell.textLabel?.text = universities[indexPath.row]
+        return cell
+    }
 
 }
 
